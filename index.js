9050d867b7f9283f0be738f2d31a6f020175a2b8
Marina Kuznetsova <mkzntsva@users.noreply.github.com>
2023-11-16 13:37:32 +0200

    Üheksas tund on lõppenud

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const bodyparser = require('body-parser');
 const dateInfo = require('./date_time_et');
 const dbConfig = require('../../vp23config');
 const database = 'if23_marina_kuznetsova';
-
+const multer = require('multer');
+//seame multer jaoks vahevara, mis määrab üleslaadimise kataloogi
+const upload = multer({dest: './public/gallery/orig/'});
+const mime = require('mime');
+const sharp = require ('sharp');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyparser.urlencoded({extended: false}));
+//app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({extended: true}));
 
 //loon andmebaasiühenduse
-const conn = mysql.createConnection({
+const connection = mysql.createConnection({
 	host: dbConfig.configData.host,
 	user: dbConfig.configData.user,
 	password: dbConfig.configData.password,
@@ -88,6 +93,50 @@ app.get('/news/read', (req,res)=> {
 	res.render('readnews');
 });
 
+app.get('/photoupload', (req, res)=> {
+	res.render('photoupload');
+});
+
+app.post('/photoupload', upload.single('photoInput'), (req, res)=> {
+	let notice = '';
+	console.log(req.file);
+	console.log(req.body);
+	//const mimeType = mime.getType(req.file.path);
+	//console.log(mimeType);
+	const fileName = 'vp_' + Date.now() + '.jpg';
+	//fs.rename(req.file.path, './public/gallery/orig/' + req.file.originalname, (err)=> {
+	fs.rename(req.file.path, './public/gallery/orig/' + fileName, (err)=> {
+		console.log('Viga: ' + err);
+	});
+	const mimeType = mime.getType('./public/gallery/orig/' + fileName);
+	console.log('Tüüp: ' + mimeType);
+	//loon pildist pisipildi (thumbnail)
+	sharp('./public/gallery/orig/' + fileName).resize(800,600).jpeg({quality : 90}).toFile('./public/gallery/normal/' + fileName);
+	sharp('./public/gallery/orig/' + fileName).resize(100,100).jpeg({quality : 90}).toFile('./public/gallery/thumbs/' + fileName);
+	
+	
+	let sql = 'INSERT INTO vp_gallery (filename, originalname, alttext, privacy, userid) VALUES (?,?,?,?,?)';
+	const userid = 1;
+	connection.query(sql, [fileName, req.file.originalname, req.body.altInput, req.body.privacyInput, userid], (err, result)=>{
+		if(err) {
+			throw err;
+			notice = 'Foto andmete salvestamine ebaõnnestus!' + err;
+			res.render('photoupload', {notice: notice});
+		}
+		else {
+			notice = 'Pilt "' + req.file.originalname + '" laeti üles!';
+			res.render('photoupload', {notice: notice});
+		}
+	});
+	
+	
+});
+
+app.get('/photogallery', (req, res)=>{
+	//res.send("See töötab!");
+	res.render('photogallery');
+});
+
 app.get('/news/read/:id/:color', (req,res)=> {
 	//res.render('readnews');
 	console.log(req.params);
@@ -106,7 +155,8 @@ app.post('/eestifilm/lisapersoon', (req, res)=>{
 			res.render('eestifilmaddperson', {notice: notice});			}
 		else{
 			notice = 'Filmitegelase ' + req.body.firstNameInput + ' ' + req.body.lastNameInput + ' salvestamine õnnestus!';
-		res.render('eestifilmaddperson', {notice: notice});			}
+		res.render('eestifilmaddperson', {notice: notice});			
+		}
 	});
 });
 
